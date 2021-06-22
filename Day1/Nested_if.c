ae68f53e3e93568057c8356823b3f2ea2ff41bee
Ashwami <86033060+Ashwami@users.noreply.github.com>
2021-06-22 23:45:35 +0530

    Add files via upload

---
+++ Day1/Nested_if.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+  Printf("First line\n");
+/* line 1 to be ignored*/
+ printf("Second line\n");
+ /* line 2 to b ignored
+   /* line 3 to b ignored */
+ */
+ return 0;
+ }
+ 
+
+
+
+
+
+
+ 
+
+
